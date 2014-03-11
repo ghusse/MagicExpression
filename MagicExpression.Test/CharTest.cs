caa5f1992a6605ac0416cfb92baeded0720f8c82
Guillaume Gautreau <guillaume+github@ghusse.com>
2014-03-11 21:35:27 +0100

    Unit tests

--- MagicExpression.Test/CharTest.cs
+++ MagicExpression.Test/CharTest.cs
@@ -29,5 +29,68 @@
 
 			this.AssertIsMatching(".", "$", "{", "[", "(", "|", "*", "+", "?", @"\", ")");
 		}
+
+		[TestMethod]
+		public void CharacterNotInTest()
+		{
+			this.Magic.CharacterNotIn(Characters.LowerCaseLetter, "%");
+
+			this.AssertIsMatching("L", "9", "$");
+			this.AssertIsNotMatching("a", "z", "%");
+		}
+
+		[TestMethod]
+		public void OneCharTest()
+		{
+			this.Magic.Character('a');
+
+			this.AssertIsMatching("ab", "ba");
+			this.AssertIsNotMatching("b");
+		}
+
+		[TestMethod]
+		public void CharListTest()
+		{
+			this.Magic.CharacterIn('a', 'b');
+
+			this.AssertIsMatching("ac", "bc");
+			this.AssertIsNotMatching("cd");
+		}
+
+		[TestMethod]
+		public void CharListNotInTest()
+		{
+			this.Magic.CharacterNotIn('a', 'b');
+
+			this.AssertIsMatching("ac", "cb");
+			this.AssertIsNotMatching("ab");
+		}
+
+		[TestMethod]
+		public void CharStringTest()
+		{
+			this.Magic.CharacterIn("abcde");
+
+			this.AssertIsMatching("az", "9b");
+			this.AssertIsNotMatching("z9");
+		}
+
+		[TestMethod]
+		public void CharNotInStringTest()
+		{
+			this.Magic.CharacterNotIn("abcde");
+
+			this.AssertIsMatching("az", "zb");
+			this.AssertIsNotMatching("ab", "bc");
+		}
+
+		[TestMethod]
+		public void AnyCharTest()
+		{
+			this.Magic.Character();
+
+			this.AssertIsMatching("0", "o", "^");
+			this.AssertIsNotMatching(string.Empty);
+		}
 	}
 }
