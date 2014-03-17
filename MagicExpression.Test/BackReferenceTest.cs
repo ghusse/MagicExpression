a8f06c1d32228e4f4c2bd3c8cfc3e75da309c78a
Guillaume Gautreau <guillaume+github@ghusse.com>
2014-03-17 09:23:42 +0100

    Unit tests

---
+++ MagicExpression.Test/BackReferenceTest.cs
@@ -0,0 +1,41 @@
+﻿namespace MagicExpression.Test
+{
+	using System;
+	using System.Text;
+	using System.Collections.Generic;
+	using System.Linq;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	/// <summary>
+	/// Summary description for BackReferenceTest
+	/// </summary>
+	[TestClass]
+	public class BackReferenceTest : MagicTest
+	{
+		[TestInitialize]
+		public override void Setup()
+		{
+			base.Setup();
+		}
+
+		[TestMethod]
+		public void IndexedTest()
+		{
+			this.Magic.Capture(x => x.String("abcd"))
+				.BackReference(1);
+
+			this.AssertIsMatching("abcdabcd", "aabcdabcdd");
+			this.AssertIsNotMatching("abcd");
+		}
+
+		[TestMethod]
+		public void NamedTest()
+		{
+			this.Magic.CaptureAs("myCapture", x => x.String("ab"))
+				.BackReference("myCapture");
+
+			this.AssertIsMatching("abab", "aababb");
+			this.AssertIsNotMatching("ab", "aba");
+		}
+	}
+}
