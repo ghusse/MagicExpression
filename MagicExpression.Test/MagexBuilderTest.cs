f4fb5d37cc13f748c5b9bc2169a1e20d57753eff
Tim Bourguignon <timothepdotnet@gmail.com>
2014-03-17 15:29:31 +0100

    Ignored test and added todo comments

--- MagicExpression.Test/MagexBuilderTest.cs
+++ MagicExpression.Test/MagexBuilderTest.cs
@@ -19,5 +19,17 @@ namespace MagicExpression.Test
 			this.AssertIsMatching("0", "9", "20", "42");
 			this.AssertIsNotMatching("43", "52");
     }
+
+		[Ignore]	//TODO: currently doesn't work because of the \bEXP\b added by the range builder
+							//TODO: also doesn't allow leading zeros
+							//TODO: also shouldn't allow leading/trailing numbers
+		[TestMethod]
+		public void RangeBetweenTest()
+		{
+			this.Magic.Character('a').Builder.NumericRange(0, 42).Character('a');
+
+			this.AssertIsMatching("a0a", "a9a", "a20a", "a42a");
+			this.AssertIsNotMatching("", "%", "a9b", "b52a");
+		}
   }
 }
