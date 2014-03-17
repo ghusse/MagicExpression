fb72be9535fdd1cfebfbb89cbe166f3205eee1db
Guillaume Gautreau <guillaume+github@ghusse.com>
2014-03-17 09:35:19 +0100

    Unit tests

---
+++ MagicExpression.Test/LineTest.cs
@@ -0,0 +1,35 @@
+﻿namespace MagicExpression.Test
+{
+	using System;
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+	[TestClass]
+	public class LineTest : MagicTest
+	{
+		[TestInitialize]
+		public override void Setup()
+		{
+			base.Setup();
+		}
+
+		[TestMethod]
+		public void StartTest()
+		{
+			this.Magic.StartOfLine()
+				.String("abc");
+
+			this.AssertIsMatching("abc");
+			this.AssertIsNotMatching("aabc");
+		}
+
+		[TestMethod]
+		public void EndTest()
+		{
+			this.Magic.String("abc").EndOfLine();
+
+			this.AssertIsMatching("abc", "aaaabc");
+			this.AssertIsNotMatching("abcc");
+		}
+	}
+}
